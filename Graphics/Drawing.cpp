6580d1f56dfda8cc450410b9b0d9174a6e20394f
Razzor Makaveli <34600323+0razzor0@users.noreply.github.com>
2020-04-09 19:53:11 +0200

    Add files via upload

---
+++ Graphics/Drawing.cpp
@@ -0,0 +1,7 @@
+/*#include "Drawing.h"
+void Drawing::draw_line(GLfloat x1, GLfloat y1,GLfloat x2,GLfloat y2){
+}
+void Drawing::draw_rectangle(GLfloat x1, GLfloat y1,GLfloat x2, GLfloat y2, bool outline){
+
+}
+*/
