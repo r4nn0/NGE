d0c5c89ec157322622ff0fc466226a88ef8833b9
Makaveli~ <ranno@grodriket.com>
2021-09-13 21:08:50 +0200

    Some Cleanup + Vulkan Support

--- NGE-Linux/build-dbg.sh
+++ NGE-Linux/build-dbg.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-g++ -Wall -g -Iinclude -Iinclude -c Engine.cpp -o obj/Debug/Engine.o
-g++ -Wall -g -Iinclude -Iinclude -c gmath.cpp -o obj/Debug/gmath.o
-g++ -Wall -g -Iinclude -Iinclude -c Graphics/Renderer2D.cpp -o obj/Debug/Graphics/Renderer2D.o
-g++ -Wall -g -Iinclude -Iinclude -c Graphics/Sprite.cpp -o obj/Debug/Graphics/Sprite.o
-g++ -Wall -g -Iinclude -Iinclude -c Graphics/stb_image.cpp -o obj/Debug/Graphics/stb_image.o
-g++ -Wall -g -Iinclude -Iinclude -c Input.cpp -o obj/Debug/Input.o
-g++ -Wall -g -Iinclude -Iinclude -c main.cpp -o obj/Debug/main.o
-g++ -Wall -g -Iinclude -Iinclude -c ngetype.cpp -o obj/Debug/ngetype.o
-g++ -o bin/NGE-Debug.bin obj/Debug/Engine.o obj/Debug/gmath.o obj/Debug/Graphics/Renderer2D.o obj/Debug/Graphics/Sprite.o obj/Debug/Graphics/stb_image.o obj/Debug/Input.o obj/Debug/main.o obj/Debug/ngetype.o -lftgl -lfreetype -lGL -lGLEW -lglfw
+g++ -Wall -g -c Engine.cpp -o obj/Debug/Engine.o
+g++ -Wall -g -c gmath.cpp -o obj/Debug/gmath.o
+g++ -Wall -g -c Graphics/Renderer2D.cpp -o obj/Debug/Graphics/Renderer2D.o
+g++ -Wall -g -c Graphics/Sprite.cpp -o obj/Debug/Graphics/Sprite.o
+g++ -Wall -g -c Graphics/stb_image.cpp -o obj/Debug/Graphics/stb_image.o
+g++ -Wall -g -c Input.cpp -o obj/Debug/Input.o
+g++ -Wall -g -c main.cpp -o obj/Debug/main.o
+g++ -Wall -g -c ngetype.cpp -o obj/Debug/ngetype.o
+g++ -o bin/NGE-Debug.bin obj/Debug/Engine.o obj/Debug/gmath.o obj/Debug/Graphics/Renderer2D.o obj/Debug/Graphics/Sprite.o obj/Debug/Graphics/stb_image.o obj/Debug/Input.o obj/Debug/main.o obj/Debug/ngetype.o -lftgl -lfreetype -lGL -lGLEW -lvulkan -lglfw
--- NGE-Linux/prerequisite.sh
+++ NGE-Linux/prerequisite.sh
@@ -4,3 +4,5 @@ sudo apt-get install libfreetype6-dev
 sudo apt-get install libgl1-mesa-dev
 sudo apt-get install libglew-dev
 sudo apt-get install libglfw3-dev
+sudo apt-get install libvulkan-dev
+sudo apt-get install libglm-dev
