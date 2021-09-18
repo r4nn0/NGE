15559e1577dd6bd5dba14fd88bd50cff754a46d7
Makaveli~ <ranno@grodriket.com>
2021-09-18 20:53:52 +0200

    Minor Update

--- Mingw/main.cpp
+++ Mingw/main.cpp
@@ -17,11 +17,16 @@ int main (){
 
 	Renderer2D renderer;
 
-    Sprite spr("./Assets/Sprites/sonic.png", glm::vec3(100, 64, 0));
-    Sprite spr2("./Assets/Sprites/sprWall.png", glm::vec3(64, 64, 0));
-
-
-
+    TexturePage tp;
+    //After Creating the tp you need to resize it so you can load sprites to it
+    //ex: tp.ImageResizeCanvas(100/*width*/, 100/*height*/,4/*number of channels*/ );
+    //Now you need to create a sprite and pass it to the tp
+    //ex: Sprite spr("C:/Path/To/Sprite.png", glm::vec3(0,0,0)/*Position: where it should be rendered*/);
+    //tp.ImageAdd(&spr);
+    //Don't forget to bind the texture in the tp (tp.Bind(0/*Texture Slot, Range from 0 to 32*/);)
+    //If you haven't bound any other texture you don't need to worry about the last step
+    //Scroll down to pass the sprite to the renderer
+    
     std::string vshader=gameEngine->LoadShaderFromFile("./Graphics/Shaders/shader.vs"),
                 fshader=gameEngine->LoadShaderFromFile("./Graphics/Shaders/shader.fs");
     unsigned int shader=gameEngine->CreateShader(vshader.c_str(),fshader.c_str());
@@ -30,15 +35,11 @@ int main (){
     double prevTime = glfwGetTime();
     unsigned short FPS = 0;
     std::string fpsString = "0";
-    TexturePage tp;
-    tp.ImageResizeCanvas(90, 50, 4);
-    tp.ImageAdd(&spr);
-    tp.ImageAdd(&spr2);
-    tp.Bind(0);
+    
     while(!glfwWindowShouldClose(gameEngine->get_window())){
         if (keyboard_check_pressed(GLFW_KEY_ESCAPE))
             break;
-
+       
         double currTime = glfwGetTime();
         FPS++;
         if ( currTime - prevTime >= 1.0 ){
@@ -46,7 +47,7 @@ int main (){
             FPS=0;
             prevTime=currTime;
         }
-
+        
         gameEngine->background_color=BACKGROUND_COLOR;
         gameEngine->StepEvent();
         gameEngine->BeginDraw();
@@ -54,17 +55,16 @@ int main (){
         font.FaceSize(16);
         font.Render(fpsString.c_str(),-1,FTPoint(0,600-font.FaceSize(),0));
 
-
+        
         glUseProgram(shader);
         glUniformMatrix4fv(glGetUniformLocation(shader, "proj_matrix"),1,GL_FALSE,gameEngine->getOthroMatrix());
         glUniform1i(glGetUniformLocation(shader, "texture"), 0);
-
+        
         renderer.renderBegin();
-
-        for (int i = 0;i < 1000;i++) {
-            renderer.addSprite(&spr);
-            renderer.addSprite(&spr2);
-        }
+        //renderer.addSprite(&spr);
+        //Here you can render sprites depending on MAX_SPRITE_COUNT macro in Renderer2D.h
+        //changing the number won't affect the performance but it won't render anything
+        //if you render more sprites than MAX_SPRITE_COUNT and it might crash
         renderer.renderEnd();
         renderer.Render();
 
@@ -74,7 +74,7 @@ int main (){
         gameEngine->EndDraw();
         renderer.dcpf = 0;
     }
-
+    
     glDeleteProgram(shader);
     glfwTerminate();
     return 0;
