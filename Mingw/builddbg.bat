04a63858cc62051e357b93a47f0058c0fa0059f4
Makaveli~ <ranno@grodriket.com>
2021-09-20 23:03:27 +0200

    .

--- Mingw/builddbg.bat
+++ Mingw/builddbg.bat
@@ -7,4 +7,4 @@ g++.exe -Wall -DGLEW_STATIC -g -Iinclude -c Graphics\stb_image.cpp -o obj\Debug\
 g++.exe -Wall -DGLEW_STATIC -g -Iinclude -c Input.cpp -o obj\Debug\Input.o
 g++.exe -Wall -DGLEW_STATIC -g -Iinclude -c main.cpp -o obj\Debug\main.o
 g++.exe -Wall -DGLEW_STATIC -g -Iinclude -c ngetype.cpp -o obj\Debug\ngetype.o
-g++.exe -Llib -o bin\Debug\NGE.exe obj\Debug\Engine.o obj\Debug\gmath.o obj\Debug\Graphics\Renderer2D.o obj\Debug\Graphics\Sprite.o obj\Debug\Graphics\TexturePage.o obj\Debug\Graphics\stb_image.o obj\Debug\Input.o obj\Debug\main.o obj\Debug\ngetype.o -lftgl_static -lvulkan-1 -lfreetype glew32s -lglfw3 -lgdi32 -lwinmm -lopengl32
+g++.exe -Llib -o bin\Debug\NGE.exe obj\Debug\Engine.o obj\Debug\gmath.o obj\Debug\Graphics\Renderer2D.o obj\Debug\Graphics\Sprite.o obj\Debug\Graphics\TexturePage.o obj\Debug\Graphics\stb_image.o obj\Debug\Input.o obj\Debug\main.o obj\Debug\ngetype.o -lftgl_static -lfreetype -lglew32s -lglfw3 -lgdi32 -lwinmm -lopengl32
--- Mingw/buildrel.bat
+++ Mingw/buildrel.bat
@@ -7,4 +7,4 @@ g++.exe -Wall -DGLEW_STATIC -O2 -Iinclude -c Graphics\stb_image.cpp -o obj\Relea
 g++.exe -Wall -DGLEW_STATIC -O2 -Iinclude -c Input.cpp -o obj\Release\Input.o
 g++.exe -Wall -DGLEW_STATIC -O2 -Iinclude -c main.cpp -o obj\Release\main.o
 g++.exe -Wall -DGLEW_STATIC -O2 -Iinclude -c ngetype.cpp -o obj\Release\ngetype.o
-g++.exe -Llib -o bin\Release\NGE.exe obj\Release\Engine.o obj\Release\gmath.o obj\Release\Graphics\Renderer2D.o obj\Release\Graphics\Sprite.o obj\Release\Graphics\TexturePage.o obj\Release\Graphics\stb_image.o obj\Release\Input.o obj\Release\main.o obj\Release\ngetype.o  -s  -lftgl_static -lfreetype libglew32s -lglfw3 -lgdi32 -lwinmm -lopengl32
+g++.exe -Llib -o bin\Release\NGE.exe obj\Release\Engine.o obj\Release\gmath.o obj\Release\Graphics\Renderer2D.o obj\Release\Graphics\Sprite.o obj\Release\Graphics\TexturePage.o obj\Release\Graphics\stb_image.o obj\Release\Input.o obj\Release\main.o obj\Release\ngetype.o  -s  -lftgl_static -lfreetype -lglew32s -lglfw3 -lgdi32 -lwinmm -lopengl32
