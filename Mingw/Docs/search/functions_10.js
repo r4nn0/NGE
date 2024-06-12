4605e2c26a0949ede88136f6ae1a493e59e9fda2
Makaveli~ <34600323+r4nn0@users.noreply.github.com>
2024-06-12 13:09:52 +0200

    Updated the docs

--- Mingw/Docs/search/functions_10.js
+++ Mingw/Docs/search/functions_10.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['texturepage_0',['TexturePage',['../class_texture_page.html#a3cad583cda01aa01319e8f0ce5de5ce7',1,'TexturePage']]]
+  ['texturepage_0',['TexturePage',['../class_texture_page.html#abaa0565c2922c6165e4755b4929fb106',1,'TexturePage::TexturePage(int=0)'],['../class_texture_page.html#a8971e103aca43ce1c8bf52c60be1ccba',1,'TexturePage::TexturePage(int, int, int=4, int=0)']]]
 ];
