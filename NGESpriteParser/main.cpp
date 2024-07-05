71e350e30bfba478c391cfa5339022b152a61306
Makaveli~ <34600323+r4nn0@users.noreply.github.com>
2024-07-05 13:10:33 +0200

    Sprite parser ready to use

--- NGESpriteParser/main.cpp
+++ NGESpriteParser/main.cpp
@@ -17,36 +17,46 @@ int main(int argc, char* argv[]){
     std::string outName="";
     std::string outData="";
     int i=0;
-    std::vector<char*> dataTotal;
-    char* data = (char*)malloc(1);
+    int count=0;
+    std::vector<std::vector<char>> dataTotal;
     unsigned sizeTot=0;
     while(argc){
         i++;
+        std::cout << argv[i] << std::endl;
         if(!strcmp(argv[i], "-o")){
             outName=argv[i+1];
             i+=2;
+            argc-=2;
+            continue;
         }
-        std::ifstream in(argv[i], std::ios::binary | std::ios::in);
+        std::ifstream in(argv[i], std::ios::binary);
+        std::vector<char> data;
         in.seekg(0, std::ios::end);
-        unsigned int size=in.tellg();
+        unsigned long long size=in.tellg();
         in.seekg(0, std::ios::beg);
-        sizeTot+=size;
-        std::cout << sizeTot << std::endl;
-        data = (char*)realloc(data, sizeTot);
-        in.read(&data[sizeTot-size], size);
+
+        data.resize(size);
+        in.read(&data[0], size);
         in.close();
+        dataTotal.push_back(data);
         argc--;
+        count++;
     }
-    if(outName=="") outName=argv[1];
+    if(outName=="") {
+        outName=argv[1];
+        outName=outName.substr(0,outName.find_last_of("."));
+    }
     outData+=(char)outName.length();
     outData+=outName;
-    outData+=(char)i;
+    outData+=(char)count;
     outName+=".ngesprite";
-    
+    for(int frames=0;frames<dataTotal.size();frames++){
         outData+="BEG";
-        outData+=data;
+        for(int temp=0;temp<dataTotal[frames].size();temp++){
+            outData+=dataTotal[frames][temp];
+        }
         outData+="END";
-        
+    }   
     std::ofstream out(outName, std::ios::out | std::ios::binary);
     out.write(outData.c_str(), outData.length());
     out.close();
